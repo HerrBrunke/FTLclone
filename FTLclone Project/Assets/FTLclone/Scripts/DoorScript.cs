cc6815b689bda14578148e94f6dd3ac6dda8fbdf
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-01-13 15:28:44 +0100

    Added Doors Added Skycube

---
+++ FTLclone Project/Assets/FTLclone/Scripts/DoorScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    Animator animator;
+    bool doorOpen;
+
+    private void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            doorOpen = true;
+            Doors("Open");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            Doors("Close");
+        }
+    }
+    void Doors(string direction)
+    {
+        animator.SetTrigger(direction);
+    }
+}
